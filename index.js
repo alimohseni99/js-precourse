447cfe81cbde19570e07d18e08009bab59c86727
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-09-22 11:51:06 +0200

    add and remove element

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ myFunctions = {
   },
 };
 console.log('salt> Functions loaded');
+
+function addListInMain(numberOfListItems) {
+  const ulElement = document.createElement('ul');
+
+  for (let i = 0; i < numberOfListItems; i++) {
+    const liItem = document.createElement('li');
+    liItem.textContent = `Item #${i + 1}`;
+
+    ulElement.appendChild(liItem);
+  }
+
+  const main = document.querySelector('main');
+  main.appendChild(ulElement);
+}
+function removeDivById(id) {
+  const divToRemove = document.querySelector('div#' + id);
+  if (divToRemove != undefined) {
+    divToRemove.remove();
+  }
+}
