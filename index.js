f11f47f3ae79f6517cccf8bacafffd7c9f502741
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-09-22 10:45:03 +0200

    elementbyID

--- index.js
+++ index.js
@@ -4,13 +4,3 @@ myFunctions = {
   },
 };
 console.log('salt> Functions loaded');
-
-const companyName = myFunctions.getCompanyName();
-document.getElementById('companyName').innerHTML = companyName;
-
-const boldNodes = document.getElementsByClassName('bold');
-
-console.log(document.getElementById('companyName'));
-for (let i = 0; i < boldNodes.length; i++) {
-  boldNodes[i].innerHTML += ' ... and bold';
-}
