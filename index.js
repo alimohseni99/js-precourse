1a9fe58dbdcbfec373edb2b8b08cda2c37edcb63
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-09-22 10:28:05 +0200

    js on diff file

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ myFunctions = {
   },
 };
 console.log('salt> Functions loaded');
+
+const companyName = myFunctions.getCompanyName();
+document.getElementById('companyName').innerHTML = companyName;
+
+const boldNodes = document.getElementsByClassName('bold');
+
+console.log(document.getElementById('companyName'));
+for (let i = 0; i < boldNodes.length; i++) {
+  boldNodes[i].innerHTML += ' ... and bold';
+}
